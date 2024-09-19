8d205fa1549e8a5868f14cb65b509399395f0129
Rohit Kumar <kumar1rohit@outlook.com>
2024-09-20 00:42:05 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  const API_URL = process.env.VITE_API_URL || 'http://localhost:3000'
+
   return {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': API_URL
+        '/api': process.env.VITE_API_URL || "http://localhost:3000"
       }
     }
   }
