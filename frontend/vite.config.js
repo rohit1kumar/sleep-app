0395e7161c8a080b102db915d8d42486e4c9b69d
Rohit Kumar <kumar1rohit@outlook.com>
2024-09-20 00:48:06 +0530

    hardcode proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(() => {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': process.env.VITE_API_URL || "http://localhost:3000"
+        '/api': "https://sleep-api.onrender.com" || "http://localhost:3000"
       }
     }
   }
