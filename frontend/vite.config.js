286d8d35ea8486f260139d51a60955bda73445ff
Rohit Kumar <kumar1rohit@outlook.com>
2024-09-19 23:57:21 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:3000'}`;
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': API_URL
+      }
     }
   }
 })
