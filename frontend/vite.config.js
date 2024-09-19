e1b588d8dd0368cea7acff270ba09fefacc66bbf
Rohit Kumar <kumar1rohit@outlook.com>
2024-09-20 00:28:46 +0530

    use process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:3000'}`;
+export default defineConfig(() => {
+  const API_URL = process.env.VITE_API_URL || 'http://localhost:3000'
   return {
     plugins: [react()],
     server: {
